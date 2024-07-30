adc4773765a49d8a701574608aee531c4fcd060d
Maxim Abrosimov <mabrosimov@phoenixit.ru>
2024-07-30 18:20:04 +0300

    add collapse to storybook

---
+++ src/stories/MCollapse.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryFn, StoryObj } from '@storybook/vue3';
+import MCollapse from '@/components/MCollapse/MCollapse.vue';
+
+const meta = {
+  title: 'Example/MCollapse',
+  component: MCollapse,
+  tags: ['autodocs'],
+  argTypes: {
+    modelValue: { control: 'text' },
+    toTeleport: { control: 'text' }
+  },
+  args: {
+    modelValue: false,
+  }
+} satisfies Meta<typeof MCollapse>;
+
+const Template: StoryFn<typeof MCollapse> = (args) => ({
+  components: { MCollapse },
+  setup() {
+    return { args };
+  },
+  template: `
+    <MCollapse>
+      <template #default="{ toggleCollapse }">
+        <div @click="toggleCollapse">trigger</div>
+      </template>
+      <template #expand>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam assumenda at,
+        consequatur corporis delectus dignissimos doloremque fuga inventore magnam nostrum odit
+        optio tenetur. Commodi doloremque ipsa nostrum possimus vel.
+      </template>
+    </MCollapse>
+  `
+});
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = Template.bind({});
+Primary.args = {
+  modelValue: false
+};
