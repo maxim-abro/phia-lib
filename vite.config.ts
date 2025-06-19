27f1b3c90c2fc80ad4c55258a9804eabd2a36bb8
Maxim Abrosimov <mabrosimov@phoenixit.ru>
2025-06-19 17:44:49 +0300

    build: setting vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJsPlugin({ useStrictCSP: true, relativeCSSInjection: false }),
     dts({
       include: [
         'src/index.ts',
@@ -29,6 +31,7 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
     lib: {
+      name: 'phia-ui',
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
       fileName: () => {
@@ -38,6 +41,7 @@ export default defineConfig({
     rollupOptions: {
       external: ['vue'],
       output: {
+        exports: 'named',
         globals: {
           vue: 'Vue'
         }
